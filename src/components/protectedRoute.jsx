17edce2e9a30e8947b030805226251409a83bb86
Alan Paccor <alanpaccor@gmail.com>
2024-05-24 00:45:50 -0400

    alert for people to login

--- src/components/protectedRoute.jsx
+++ src/components/protectedRoute.jsx
@@ -1,5 +1,23 @@
-import { Navigate } from "react-router-dom"
+import React from "react";
+import './popup.css'; // Import the CSS file
 
-export const ProtectedRoute = ({children, user}) => {
-    return user ? children : <Navigate to={'/'}></Navigate>
-}
+export const ProtectedRoute = ({ children, user }) => {
+  const handleProtectedRouteClick = () => {
+    if (!user) {
+      alert("Please log in to access this page.");
+    }
+  };
+
+  return user ? (
+    children
+  ) : (
+    <section>
+            <form className="loginPage2">
+                <fieldset className="fieldset2">
+                     <p className="input2" >😡 Please login to an account to be able to play 💙</p>
+                     <a href="/login" type="button" className="loginPageButton2" >Log In</a>
+                </fieldset>
+            </form>
+        </section>
+  );
+};
