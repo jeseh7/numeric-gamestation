d7e3c22c5c92c98f378fd52c6b3ccd170ce32fd7
Alan Paccor <alanpaccor@gmail.com>
2024-06-01 16:58:36 -0400

    fixed the profile square on the top right

--- src/Landing.js
+++ src/Landing.js
@@ -57,7 +57,7 @@ function Landing() {
       {currentUser && (
         <div className={`dropdownContainer ${theme === 'dark' ? 'darkTheme' : ''}`}>
           <Dropdown>
-            <Dropdown.Toggle variant={theme === 'dark' ? 'light' : 'dark'} id="dropdown-basic" className="dropdownToggle rounded">
+            <Dropdown.Toggle variant={theme === 'dark' ? 'light' : 'dark'} id="dropdown-basic" className="dropdownToggle rounded" style={{ padding: '0.05rem 0.2rem', fontSize: '0.975rem' }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
               </svg>
@@ -69,6 +69,7 @@ function Landing() {
           </Dropdown>
         </div>
       )}
+
 
       <video className="App-video" id='numeric' muted playsInline autoPlay loop style={{ filter: theme === 'dark' ? 'invert(85%)' : 'none' }}>
           <source src={numerical} type="video/mp4" />
